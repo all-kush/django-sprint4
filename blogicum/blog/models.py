0ab29af022669604bd2dde9242ae769696d17c5e
Алена Кушнерова <kushnerovaalyona@yandex.ru>
2026-05-10 07:15:21 +0300

    django-sprint4_version_1

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -91,13 +91,11 @@ class Comment(models.Model):
     post = models.ForeignKey(
         'Post',
         on_delete=models.CASCADE,
-        related_name='comments'
-        )
+        related_name='comments')
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='comments'
-        )
+        related_name='comments')
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
